484cb95d7a503b7570169ad70c314b03eb60fb7e
Frost15777 <di.yu.mk@gmail.com>
2024-03-16 14:57:05 +0100

    update-1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,55 +1,80 @@
-const { listContacts, getContactById, removeContact, addContact, updateContact } = require('/services/contactsServices');
-const { validateContact, validateUpdateContact } = require('/schemas/contactsSchemas');
+import * as contactsServices from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
 
-const getAllContacts = (req, res) => {
-    const contacts = listContacts();
-    res.status(200).json(contacts);
-};
-
-const getContact = (req, res) => {
-    const { id } = req.params;
-    const contact = getContactById(id);
-    if (contact) {
-        res.status(200).json(contact);
-    } else {
-        res.status(404).json({ message: "Not found" });
-    }
-};
-
-const deleteContact = (req, res) => {
-    const { id } = req.params;
-    const deletedContact = removeContact(id);
-    if (deletedContact) {
-        res.status(200).json(deletedContact);
-    } else {
-        res.status(404).json({ message: "Not found" });
-    }
-};
-
-const createContact = (req, res) => {
-    const { name, email, phone } = req.body;
-    const { error } = validateContact(req.body);
-    if (error) {
-        res.status(400).json({ message: error.message });
-    } else {
-        const newContact = addContact(name, email, phone);
+const createContact = async (req, res, next) => {
+    try {
+        const { name, email, phone } = req.body;
+        const { error } = createContactSchema.validate(req.body);
+        if (error) {
+            throw HttpError(400, error.message);
+        }
+        const newContact = await contactsServices.addContact(name, email, phone);
         res.status(201).json(newContact);
+    } catch (error) {
+        next(error);
     }
 };
 
-const updateContactById = (req, res) => {
-    const { id } = req.params;
-    const { error } = validateUpdateContact(req.body);
-    if (error) {
-        res.status(400).json({ message: error.message });
-    } else {
-        const updatedContact = updateContact(id, req.body);
+const getAllContacts = async (req, res, next) => {
+    try {
+        const contacts = await contactsServices.listContacts();
+        res.status(200).json(contacts);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getOneContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const contact = await contactsServices.getContactById(id);
+        if (contact) {
+            res.status(200).json(contact);
+        } else {
+            throw HttpError(404, "Not found");
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deletedContact = await contactsServices.removeContact(id);
+        if (deletedContact) {
+            res.status(200).json(deletedContact);
+        } else {
+            throw HttpError(404, "Not found");
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { error } = updateContactSchema.validate(req.body);
+        if (error) {
+            throw HttpError(400, error.message);
+        }
+        const updatedContact = await contactsServices.updateContact(id, req.body);
         if (updatedContact) {
             res.status(200).json(updatedContact);
         } else {
-            res.status(404).json({ message: "Not found" });
+            throw HttpError(404, "Not found");
         }
+    } catch (error) {
+        next(error);
     }
 };
 
-module.exports = { getAllContacts, getContact, deleteContact, createContact, updateContactById };
+export default {
+    createContact,
+    getAllContacts,
+    getOneContact,
+    deleteContact,
+    updateContact
+};
